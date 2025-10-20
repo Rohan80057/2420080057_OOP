a1924987610dc8b368d214dfa128f4854a0640fd
Rohan <2420080057@klh.edu.in>
2025-10-20 20:46:46 +0530

    Delete src directory

--- src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-import RegisterForm from '../components/Auth/RegisterForm';
-import { useAuth } from '../hooks/useAuth';
-
-export default function Register() {
-  const { login } = useAuth();
-  return <RegisterForm onRegister={login} />;
-}
--- src/utils/chartData.js
+++
@@ -1,8 +0,0 @@
-// Generates random chart data for analytics demo
-
-export function generatePieChartData(portfolio) {
-  return portfolio.map(stock => ({
-    symbol: stock.symbol,
-    value: stock.quantity * stock.price
-  }));
-}
